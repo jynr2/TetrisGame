2056fea7e340756c9172268e1629785f8e35cef9
jyn <jhonjyn1@gmail.com>
2025-04-25 17:30:27 -0500

    update

--- tetris.py
+++ tetris.py
@@ -1,5 +1,5 @@
 from Movement import Movement
-from constants import BLACK_SIDE, get_all_pieces, get_empty_screen
+from constants import BLACK_SIDE, WHITE_SIDE, get_all_pieces, get_empty_screen
 import os
 
 def print_screen(screen):
@@ -7,9 +7,10 @@ def print_screen(screen):
         print(''.join(map(str, base)))
     return screen
 
-def start_game(piece_code:str = 'j'):
+def start_game(piece_code:str = 'j', screen_base:list[list[str]] = []):
     piece = get_all_pieces()[piece_code]
-    screen_base = get_empty_screen()
+    if screen_base == []:
+        screen_base = get_empty_screen()
     for i, values in enumerate(piece):
         for j, value in enumerate(values):
             screen_base[i][j] = value
@@ -20,33 +21,59 @@ def print_piece(piece):
     for base in piece:
         print(''.join(map(str, base)))
 
-def move_piece(movement:Movement, screen:list[list[str]]):
-    new_screen = get_empty_screen()
 
-    for row_index, row in enumerate(screen):
-        for column_index, item in enumerate(row):
-            if item == BLACK_SIDE:
-                new_row_index = 0
-                new_column_index = 0
-                match movement:
-                    case Movement.DOWN:
-                        new_row_index = row_index + 1
-                        new_column_index = column_index
-                    case Movement.RIGHT:
-                        new_row_index = row_index
-                        new_column_index = column_index + 1
-                    case Movement.LEAF:
-                        new_row_index = row_index
-                        new_column_index = column_index - 1
-                    case Movement.ROTATE:
-                        pass
-                if new_row_index > 9 or new_column_index > 9 or new_column_index < 0:
-                    return (screen)
-                else:
-                    new_screen[new_row_index][new_column_index] = item
+def move_piece_right(matriz:list[list[str]]):
+    print(type(matriz))
+    for row in matriz:
+        for i in range(len(row) - 2, -1, -1):
+            if row[i] == BLACK_SIDE and row[i + 1] == WHITE_SIDE:
+                row[i + 1] = BLACK_SIDE
+                row[i] = WHITE_SIDE
+    return matriz
+
+def mover_asteriscos_derecha(matriz: list[list[str]]):
+    for row in matriz:
+        for i in range(len(row) - 2, -1, -1):
+            if row[i] == BLACK_SIDE and row[i + 1] == WHITE_SIDE:
+                row[i + 1] = BLACK_SIDE
+                row[i] = WHITE_SIDE
+    
+    return matriz
+
+def move_piece_left(matriz:list[list[str]]):
+    for row in matriz:
+        for i in range(1, len(row)):
+            if row[i] == BLACK_SIDE and row[i - 1] == WHITE_SIDE:
+                row[i - 1] = BLACK_SIDE
+                row[i] = WHITE_SIDE
+    return matriz
+
+
+def move_piece_down(matriz:list[list[str]]):
+    rows = len(matriz)
+    columns = len(matriz[0])
+    for i in range(rows - 2, -1, -1):
+        for j in range(columns):
+            if matriz[i][j] == BLACK_SIDE and matriz[i + 1][j] == WHITE_SIDE:
+                matriz[i + 1][j] = BLACK_SIDE
+                matriz[i][j] = WHITE_SIDE
+    return matriz
+
+
+def move_piece(movement:Movement, screen:list[list[str]]) -> list[list[str]]:
+    match movement:
+        case Movement.DOWN:
+            screen = move_piece_down(screen)
+        case Movement.RIGHT:
+            screen = mover_asteriscos_derecha(screen)
+        case Movement.LEAF:
+            screen = move_piece_left(screen)
+        case Movement.ROTATE:
+            pass
     os.system('clear')
-    print_screen(new_screen)
-    return new_screen
+    print_screen(screen)
+    return screen
+
 
 start_screen = start_game()
 current_screen = move_piece(Movement.RIGHT, start_screen)
@@ -56,12 +83,4 @@ current_screen = move_piece(Movement.RIGHT, current_screen)
 current_screen = move_piece(Movement.RIGHT, current_screen)
 current_screen = move_piece(Movement.RIGHT, current_screen)
 current_screen = move_piece(Movement.RIGHT, current_screen)
-
-current_screen = move_piece(Movement.DOWN, current_screen)
-current_screen = move_piece(Movement.DOWN, current_screen)
-current_screen = move_piece(Movement.DOWN, current_screen)
-current_screen = move_piece(Movement.DOWN, current_screen)
-current_screen = move_piece(Movement.DOWN, current_screen)
-current_screen = move_piece(Movement.DOWN, current_screen)
-current_screen = move_piece(Movement.DOWN, current_screen)
-current_screen = move_piece(Movement.DOWN, current_screen)
+current_screen = move_piece(Movement.RIGHT, current_screen)
