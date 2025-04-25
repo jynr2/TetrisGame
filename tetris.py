a831c10e000ba2211f90fdff59a7e00618e2d9e7
jyn <jhonjyn1@gmail.com>
2025-04-25 18:54:49 -0500

    the pieces are prevented from moving when they reach the end of X

--- tetris.py
+++ tetris.py
@@ -21,26 +21,25 @@ def print_piece(piece):
     for base in piece:
         print(''.join(map(str, base)))
 
+def is_ended(matriz:list[list[str]], index:int) -> bool:
+    first_position = list(filter(lambda fila: BLACK_SIDE in fila, matriz))[0][index]
+    last_position = list(filter(lambda fila: BLACK_SIDE in fila, matriz))[-1][index]
+    return first_position == BLACK_SIDE or last_position == BLACK_SIDE
+
 
 def move_piece_right(matriz:list[list[str]]):
-    print(type(matriz))
+    if is_ended(matriz, -1):
+        return matriz
     for row in matriz:
         for i in range(len(row) - 2, -1, -1):
             if row[i] == BLACK_SIDE and row[i + 1] == WHITE_SIDE:
                 row[i + 1] = BLACK_SIDE
                 row[i] = WHITE_SIDE
-    return matriz
-
-def mover_asteriscos_derecha(matriz: list[list[str]]):
-    for row in matriz:
-        for i in range(len(row) - 2, -1, -1):
-            if row[i] == BLACK_SIDE and row[i + 1] == WHITE_SIDE:
-                row[i + 1] = BLACK_SIDE
-                row[i] = WHITE_SIDE
-    
     return matriz
 
 def move_piece_left(matriz:list[list[str]]):
+    if is_ended(matriz, 0):
+        return matriz
     for row in matriz:
         for i in range(1, len(row)):
             if row[i] == BLACK_SIDE and row[i - 1] == WHITE_SIDE:
@@ -65,7 +64,7 @@ def move_piece(movement:Movement, screen:list[list[str]]) -> list[list[str]]:
         case Movement.DOWN:
             screen = move_piece_down(screen)
         case Movement.RIGHT:
-            screen = mover_asteriscos_derecha(screen)
+            screen = move_piece_right(screen)
         case Movement.LEAF:
             screen = move_piece_left(screen)
         case Movement.ROTATE:
@@ -75,7 +74,7 @@ def move_piece(movement:Movement, screen:list[list[str]]) -> list[list[str]]:
     return screen
 
 
-start_screen = start_game()
+start_screen = start_game('i')
 current_screen = move_piece(Movement.RIGHT, start_screen)
 current_screen = move_piece(Movement.RIGHT, current_screen)
 current_screen = move_piece(Movement.RIGHT, current_screen)
@@ -83,4 +82,32 @@ current_screen = move_piece(Movement.RIGHT, current_screen)
 current_screen = move_piece(Movement.RIGHT, current_screen)
 current_screen = move_piece(Movement.RIGHT, current_screen)
 current_screen = move_piece(Movement.RIGHT, current_screen)
+
 current_screen = move_piece(Movement.RIGHT, current_screen)
+current_screen = move_piece(Movement.RIGHT, current_screen)
+current_screen = move_piece(Movement.RIGHT, current_screen)
+
+
+current_screen = move_piece(Movement.LEAF, current_screen)
+current_screen = move_piece(Movement.LEAF, current_screen)
+current_screen = move_piece(Movement.LEAF, current_screen)
+current_screen = move_piece(Movement.LEAF, current_screen)
+current_screen = move_piece(Movement.LEAF, current_screen)
+current_screen = move_piece(Movement.LEAF, current_screen)
+current_screen = move_piece(Movement.LEAF, current_screen)
+
+current_screen = move_piece(Movement.LEAF, current_screen)
+current_screen = move_piece(Movement.LEAF, current_screen)
+current_screen = move_piece(Movement.LEAF, current_screen)
+
+
+
+
+current_screen = move_piece(Movement.DOWN, current_screen)
+current_screen = move_piece(Movement.DOWN, current_screen)
+current_screen = move_piece(Movement.DOWN, current_screen)
+current_screen = move_piece(Movement.DOWN, current_screen)
+current_screen = move_piece(Movement.DOWN, current_screen)
+current_screen = move_piece(Movement.DOWN, current_screen)
+current_screen = move_piece(Movement.DOWN, current_screen)
+current_screen = move_piece(Movement.DOWN, current_screen)
